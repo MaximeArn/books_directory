6d4a87fb39c0667cc5e8271d612bf6c30ab2ca85
MaximeArn <maxime-arnould@outlook.fr>
2023-11-05 21:39:11 +0800

    integrated book details route

--- readme.md
+++ readme.md
@@ -6,6 +6,9 @@
   - map on the json file to display the infomation of every book
     - read into the json file
     - return data in the response body
+  - add a route to get a book's information :
+    - get id from the params
+    - return the corresponding object
   - integrate a form that allow user to modify the information (writing in json file)
   - allow user to delete a book from the list
   - integrate a form to add a new book
--- server.js
+++ server.js
@@ -9,23 +9,24 @@ server.get("/", (req, res) => {
   res.send(books);
 });
 
-server.get("/book/:id", ({ params: { imageLink } }, res) => {
-  console.log(imageLink);
-  const book = data.find((book) => book.imageLink === imageLink);
+server.get("/book/:id", ({ params: { id } }, res) => {
+  console.log(id);
+  const book = books.find((book) => String(book.id) === id);
+  console.log(book);
   res.send(book);
 });
 
-server.get("/map", (req, res) => {
-  books_with_id = books.map((book, index) => {
-    book.id = index;
-    return book;
-  });
-  console.log(books_with_id);
-  res.end();
-  fs.writeFile("./books_with_id.json", JSON.stringify(books_with_id), () => {
-    console.log("done");
-  });
-});
+// server.get("/map", (req, res) => {
+//   books_with_id = books.map((book, index) => {
+//     book.id = index;
+//     return book;
+//   });
+//   console.log(books_with_id);
+//   res.end();
+//   fs.writeFile("./books_with_id.json", JSON.stringify(books_with_id), () => {
+//     console.log("done");
+//   });
+// });
 
 server.listen(port, () => {
   console.log(`server is listening on port : ${port}`);
