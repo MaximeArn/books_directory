a2cb9d5762e2c37d2167f724c415e0a0948591c6
MaximeArn <maxime-arnould@outlook.fr>
2023-11-05 21:06:42 +0800

    retrieve the list of books on default route

--- readme.md
+++ readme.md
@@ -4,6 +4,8 @@
 
 - ### First phase :
   - map on the json file to display the infomation of every book
+    - read into the json file
+    - return data in the response body
   - integrate a form that allow user to modify the information (writing in json file)
   - allow user to delete a book from the list
   - integrate a form to add a new book
--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const fs = require("fs");
+const books = require("./books.json");
 
 const port = 3000;
 const server = express();
 
 server.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.send(books);
+});
+
+server.get("/:id", ({ params: { imageLink } }, res) => {
+  console.log(imageLink);
+  const book = data.find((book) => book.imageLink === imageLink);
+  res.send(book);
 });
 
 server.listen(port, () => {
